8fced2f4434a67b3b0f66520e8bf99db4961b26f
PoutsBF <stephane.lepoutere@free.fr>
2020-04-07 19:16:08 +0200

    création

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,56 @@
 #include <Arduino.h>
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+#include <encoder.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+
+// Declaration for an SSD1306 display connected to I2C (SDA, SCL pins)
+#define OLED_RESET     4 // Reset pin # (or -1 if sharing Arduino reset pin)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void barGraph(unsigned v);
+
+void setup() 
+{
+  Serial.begin(115200);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, 0x3C)) 
+  { // Address 0x3D for 128x64
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
+}
+
+void barGraph(unsigned v) 
+{
+  char buf[20];
+  unsigned width, w;
+
+  snprintf(buf,sizeof buf,"ADC %u",v);
+  width = disp_width-2;
+  w = v * width / 4095;
+  display.print(buf);
+  display.fillRect(1,8,w,disp_height-2);
+  display.setColor(BLACK);
+  display.fillRect(w,38,disp_width-2,disp_height-2);
+  display.fillRect(1,1,disp_width-2,37);
+  display.setColor(WHITE);
+  display.drawLine(0,38,disp_width-2,38);
+  display.drawRect(0,0,disp_width-1,disp_height-1);
+  display.setTextAlignment(TEXT_ALIGN_LEFT);
+  display.setFont(ArialMT_Plain_8);
+  display.drawString(0,0,buf);
+  display.display();
 }
