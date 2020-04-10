fe60ccbc21dcd06d94b3f696141fe490260688aa
PoutsBF <stephane.lepoutere@free.fr>
2020-04-10 12:23:29 +0200

    évolution création tests d'appropriation

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,12 @@
 #include <Wire.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include "PinChangeInt.h"
 #include "encoder.h"
+
+#include "Air_Quality_Sensor.h"
+
+AirQualitySensor sensor(A3);
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
@@ -13,6 +18,12 @@
 Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void barGraph(unsigned v);
+void testdrawchar(void);
+void bargraph (uint8_t numero, uint8_t valeur);
+void TRACE(const char * texte, uint16_t valeur);
+void TRACE(const char * texte, uint16_t valeur1, uint16_t valeur2);
+void debitmetre( void );
+static uint8_t dbm_ticks = 0;
 
 const uint8_t pwmA   = 3;
 const uint8_t pwmB   = 11;
@@ -20,6 +31,10 @@ const uint8_t brakeA = 9;
 const uint8_t brakeB = 8;
 const uint8_t dirA   = 12;
 const uint8_t dirB   = 13;
+
+const uint8_t flowW  = 2;
+
+#define CSGN_VAL_MIN  128
 
 void setup() 
 {
@@ -32,6 +47,10 @@ void setup()
     for(;;); // Don't proceed, loop forever
   }
   display.clearDisplay();
+ // Show initial display buffer contents on the screen --
+  // the library initializes this with an Adafruit splash screen.  delay(2000);
+
+  testdrawchar();
 
   encoder.Timer_init();
 
@@ -46,37 +65,125 @@ void setup()
   analogWrite(pwmA, 0);
   analogWrite(pwmB, 0);
 
+  pinMode(flowW, INPUT);
+  // attachPinChangeInterrupt(4, debitmetre, CHANGE);
+  attachInterrupt(flowW, debitmetre, CHANGE);
+
+  if (sensor.init()) 
+  {
+    Serial.println("Sensor ready.");
+  } 
+  else 
+  {
+    Serial.println("Sensor ERROR!");
+  }
 }
 
 void loop() 
 {
   static uint16_t consigne = 0;
+  static int dbm_p_entree = 0;
+  static unsigned long dbm_tmps = 0;
+  static unsigned long qs_tmps = 0;
+
+  if(millis() > qs_tmps)
+  {
+    int quality = sensor.slope();
+    qs_tmps += 10000;
+
+//    Serial.print("Sensor value: ");
+//    Serial.println(sensor.getValue());
+
+    if (quality == AirQualitySensor::FORCE_SIGNAL) {
+        Serial.println("High pollution! Force signal active.");
+    } else if (quality == AirQualitySensor::HIGH_POLLUTION) {
+        Serial.println("High pollution!");
+    } else if (quality == AirQualitySensor::LOW_POLLUTION) {
+        Serial.println("Low pollution!");
+    } else if (quality == AirQualitySensor::FRESH_AIR) {
+        Serial.println("Fresh air.");
+    }
+    bargraph(3, (sensor.getValue()) >> 1);
+  }
+
+  int dbm_entree = digitalRead(flowW);
+  if(dbm_entree != dbm_p_entree)
+  {
+    unsigned long dbm_delta = millis() - dbm_tmps;
+    dbm_p_entree = dbm_entree;
+    dbm_tmps = millis();
+  
+    TRACE("Temps : %d", dbm_delta);
+  }
+
 
   if (encoder.rotate_flag == 1)
   {
     if (encoder.direct == 0)
     {
       if(consigne < 255)
-        consigne++;
+        consigne += 5;
     }
      else
     {
       if(consigne != 0)
-        consigne--;
+        consigne -= 5;
     }
-    char txt[15];
-    snprintf(txt, 15, "csgn : %3d", consigne);
-    Serial.println(txt);
 
-    display.setCursor(0, 0);
-    display.print("csgn : ");
-    display.print(consigne);
+    TRACE("\ncsgn : %3d", consigne);
 
-    analogWrite(pwmA, consigne);
-    analogWrite(pwmB, consigne);
+    if(consigne > CSGN_VAL_MIN)
+    {
+      bargraph(0, (consigne - 128) << 1);
+      analogWrite(pwmB, consigne);
+    }
+    else
+      analogWrite(pwmB, 0);
 
     encoder.rotate_flag = 0;
   }
+
+}
+
+void bargraph (uint8_t numero, uint8_t valeur)
+{
+  uint8_t posx = (numero * 8) + 2;
+  uint16_t valx = valeur + valeur + valeur;
+  valx = valx + valx + valx + valx + valx;
+  valx >>= 6; 
+
+  display.fillRect(66, posx, 60, 3, SSD1306_BLACK);
+  if(valx)
+    display.fillRect(66, posx, (uint8_t) valx, 3, SSD1306_WHITE);
+
+  display.display();
+}
+
+void testdrawchar(void) 
+{
+  display.clearDisplay();
+
+  display.setTextSize(1);      // Normal 1:1 pixel scale
+  display.setTextColor(SSD1306_WHITE); // Draw white text
+  display.cp437(true);         // Use full 256 char 'Code Page 437' font
+
+  display.setCursor(0, 0);     // Start at top-left corner
+  display.write("consigne :");
+
+  display.setCursor(0, 8);     // Start at top-left corner
+  display.write("courant :");
+
+  display.setCursor(0, 16);     // Start at top-left corner
+  display.write(("d‚bit :"));
+//  display.write(("d\x82bit :"));
+
+  display.drawRect(64, 0, 64, 7, SSD1306_WHITE);
+
+  display.drawRect(64, 8, 64, 7, SSD1306_WHITE);
+
+  display.drawRect(64, 16, 64, 7, SSD1306_WHITE);
+
+  display.drawRect(64, 24, 64, 7, SSD1306_WHITE);
 }
 
 /*
@@ -102,3 +209,16 @@ void barGraph(unsigned v)
   display.display();
 }
 */
+
+void TRACE(const char * texte, uint16_t valeur)
+{
+  char txt[30];
+  snprintf(txt, 30, texte, valeur);
+  Serial.println(txt);
+}
+
+void debitmetre( void )
+{
+  dbm_ticks++;
+  TRACE("dbm_tick : %d", dbm_ticks);
+}
